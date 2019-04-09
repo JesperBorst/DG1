e363d01183a9fedba8a3bf4f3c66ba8b6e83343b
JesperBorst <125890@student.drenthecollege.nl>
2019-04-09 11:48:59 +0200

    logo + titel

--- index.php
+++ index.php
@@ -7,13 +7,28 @@
   </head>
   <body>
 
-    <div class="navbar">
-      <ul>
-        <li> <a href="Titel.html">titel</a> </li>
-        <li> <a href="Titel.html">titel</a> </li>
-        <li> <a href="Titel.html">titel</a> </li>
-        <li> <a href="Titel.html">titel</a> </li>
-      </ul>
+      <div class="navbar">
+        <ul>
+          <li> <a href="Titel.html">titel</a> </li>
+          <li> <a href="Titel.html">titel</a> </li>
+          <li> <a href="Titel.html">titel</a> </li>
+          <li> <a href="Titel.html">titel</a> </li>
+        </ul>
+      </div>
+
+      <div id="logotitle">
+        <img id="logo-img" src = "logoforum2.png">
+        <div id="blokje"><p id="blokjetekst">SiteTitle</p></div>
+        <!-- <p id="SiteTitle">Titel</p> -->
+      </div>
+
+    <div class="titel">
+    </div>
+
+    <div class="paragraph">
+    </div>
+
+    <div id="footer">
     </div>
 
   </body>
