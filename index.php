3e90af3a3a471a85ec1510b5d4ce8cb6e974b4b1
JesperBorst <125890@student.drenthecollege.nl>
2019-04-16 08:39:08 +0200

    Paragraven en Footer

--- index.php
+++ index.php
@@ -31,10 +31,49 @@
     <div class="titel2">
       <p> topic ict & webdesign </p>
     </div>
+    <br>
 
     <div class="paragraph">
-      <p> </p>
+      <p> Lorem ipsum dolor sit amet, et pro illum decore scribentur,
+        an sed minim congue possit, id cum aliquando urbanitas.
+        Malis augue debitis mea te, iudico argumentum eam ad,
+        harum fabulas laoreet eu nam.
+        Vel liber scripta euripidis ei,
+        usu ei virtute meliore. Cu amet dignissim pri.
+        Te duo mutat vocent, pro melius verear signiferumque ei,
+        ea pri etiam repudiandae. Per et paulo sensibus,
+        an tincidunt interesset sea. Lorem ipsum dolor sit amet, et pro illum decore scribentur,
+          an sed minim congue possit, id cum aliquando urbanitas.
+          Malis augue debitis mea te, iudico argumentum eam ad,
+          harum fabulas laoreet eu nam.
+          Vel liber scripta euripidis ei,
+          usu ei virtute meliore. Cu amet dignissim pri.
+          Te duo mutat vocent, pro melius verear signiferumque ei,
+          ea pri etiam repudiandae. Per et paulo sensibus,
+          an tincidunt interesset sea.</p>
     </div>
+    <br>
+
+    <div class="paragraph">
+      <p> Lorem ipsum dolor sit amet, et pro illum decore scribentur,
+        an sed minim congue possit, id cum aliquando urbanitas.
+        Malis augue debitis mea te, iudico argumentum eam ad,
+        harum fabulas laoreet eu nam.
+        Vel liber scripta euripidis ei,
+        usu ei virtute meliore. Cu amet dignissim pri.
+        Te duo mutat vocent, pro melius verear signiferumque ei,
+        ea pri etiam repudiandae. Per et paulo sensibus,
+        an tincidunt interesset sea. Lorem ipsum dolor sit amet, et pro illum decore scribentur,
+          an sed minim congue possit, id cum aliquando urbanitas.
+          Malis augue debitis mea te, iudico argumentum eam ad,
+          harum fabulas laoreet eu nam.
+          Vel liber scripta euripidis ei,
+          usu ei virtute meliore. Cu amet dignissim pri.
+          Te duo mutat vocent, pro melius verear signiferumque ei,
+          ea pri etiam repudiandae. Per et paulo sensibus,
+          an tincidunt interesset sea.</p>
+    </div>
+    <br>
 
     <div id="footer">
     </div>
